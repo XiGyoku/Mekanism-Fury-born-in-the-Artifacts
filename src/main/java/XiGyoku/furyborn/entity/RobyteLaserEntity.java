3f1cc3094746be6bdb674edb2e852e6136b9fdf3
XiGyoku <zawaumi@gmail.com>
2026-04-03 19:50:37 +0900

    feat(robyte-refine): Added 2 EntityClasses to render things related laser.

---
+++ src/main/java/XiGyoku/furyborn/entity/RobyteLaserEntity.java
@@ -0,0 +1,27 @@
+package XiGyoku.furyborn.entity;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+
+public class RobyteLaserEntity extends Entity {
+    public RobyteLaserEntity(EntityType<?> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
+
+    @Override
+    protected void defineSynchedData() {
+
+    }
+
+    @Override
+    protected void readAdditionalSaveData(CompoundTag p_20052_) {
+
+    }
+
+    @Override
+    protected void addAdditionalSaveData(CompoundTag p_20139_) {
+
+    }
+}
